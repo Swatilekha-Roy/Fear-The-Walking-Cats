a1830a62d34932ce08e860de1b66dcf44dbbf8a7
nimit2801 <mnimitsavant@outlook.com>
2021-04-18 08:47:42 +0530

    new changes

--- js/game.js
+++ js/game.js
@@ -1,15 +1,12 @@
 let textBox = document.getElementById('text');
 let box1 = document.getElementById('button 1');
 let box2 = document.getElementById('button 2');
-textBox.textContent = 'asd';
 
-<<<<<<< HEAD
+
+
 box1.addEventListener('click', () => {
     textBox.textContent = 'some other text';    
 })
 box2.addEventListener('click', () => {
     textBox.textContent = 'nayy';    
 })
-=======
-textBox.textContent = 'asd';
->>>>>>> f18ae4cf83e8c0405403cab482663f4586df7e07
