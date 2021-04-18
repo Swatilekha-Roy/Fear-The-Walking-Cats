9d16e3c27b4ddc132087e60186a79c5d16f0697e
cyraalesha <67356675+cyraalesha@users.noreply.github.com>
2021-04-18 11:02:22 +0700

    Some of the css

--- js/game.js
+++ js/game.js
@@ -2,7 +2,12 @@ let textBox = document.getElementById('text');
 let box1 = document.getElementById('button 1');
 let box2 = document.getElementById('button 2');
 
-
+//the whole json part
+var mydata = JSON.parse(data);
+alert(mydata[0].name);
+alert(mydata[0].age);
+alert(mydata[1].name);
+alert(mydata[1].age);
 
 box1.addEventListener('click', () => {
     textBox.textContent = 'some other text';    
@@ -10,3 +15,4 @@ box1.addEventListener('click', () => {
 box2.addEventListener('click', () => {
     textBox.textContent = 'nayy';    
 })
+
