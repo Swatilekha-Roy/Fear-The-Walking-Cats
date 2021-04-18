3c463e16727bbdde2b17cdcbcce10905f40e9611
nimit2801 <mnimitsavant@outlook.com>
2021-04-18 08:40:22 +0530

    commits

--- js/game.js
+++ js/game.js
@@ -1,3 +1,11 @@
 let textBox = document.getElementById('text');
-
+let box1 = document.getElementById('button 1');
+let box2 = document.getElementById('button 2');
 textBox.textContent = 'asd';
+
+box1.addEventListener('click', () => {
+    textBox.textContent = 'some other text';    
+})
+box2.addEventListener('click', () => {
+    textBox.textContent = 'nayy';    
+})
